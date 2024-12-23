36af3ca291ca6c88bdea4cf432acc5da18157b93
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-23 08:41:17 +0530

    add http status codes utility function

---
+++ src/utils/httpStatusCodes.ts
@@ -0,0 +1,14 @@
+// Define and export an object containing HTTP status codes
+const httpStatusCodes = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    MOVED_PERMANENTLY: 301,
+    FOUND: 302,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    INTERNAL_SERVER_ERROR: 500,
+  };
+  
+  export default httpStatusCodes;
